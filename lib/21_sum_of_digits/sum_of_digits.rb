34f2796f3f9b588800cb25b71f161d13bd4e91f2
changeworld <changesworlds@gmail.com>
2011-12-10 17:55:30 +0900

    変数の統一(no, number を num へ)

--- lib/21_sum_of_digits/sum_of_digits.rb
+++ lib/21_sum_of_digits/sum_of_digits.rb
@@ -3,9 +3,8 @@
 filename = ARGV[0]
 file = File.open(filename).each do |line|
   sum_digits = 0
-  line.split(//).map {|number|
-    number = number.to_i
-    sum_digits += number
+  line.split(//).map {|num|
+    sum_digits += num.to_i
   }
   puts sum_digits
 end
--- lib/29_unique_elements/unique_elements.rb
+++ lib/29_unique_elements/unique_elements.rb
@@ -2,6 +2,6 @@
 
 filename = ARGV[0]
 file = File.open(filename).each do |line|
-  puts line.split(",").map {|no| no.to_i}.uniq.join ','
+  puts line.split(",").map {|num| num.to_i}.uniq.join ','
 end
 file.close
