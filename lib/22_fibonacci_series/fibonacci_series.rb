c4735a0c24a488260461befe3f835bbc706d169f
changeworld <changesworlds@gmail.com>
2011-12-10 17:58:22 +0900

    case から ? へ変え、省line化

--- lib/22_fibonacci_series/fibonacci_series.rb
+++ lib/22_fibonacci_series/fibonacci_series.rb
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def fibonacci i
-  case i
-  when 0
-    0
-  when 1
-    1
-  else
-    fibonacci(i - 1) + fibonacci(i - 2)
-  end
+  i == 0 ? 0 : i <= 2 ? 1 : fibonacci(i - 1) + fibonacci(i - 2)
 end
 
 filename = ARGV[0]
