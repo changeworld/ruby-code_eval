1309704a90487f48e2ace4e5ad72bc3774707537
changeworld <changesworlds@gmail.com>
2011-12-10 17:52:26 +0900

    冗長部分を削除(不要メソッドを削除)

--- lib/08_reverse_words/reverse_words.rb
+++ lib/08_reverse_words/reverse_words.rb
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 
-def reverse_words(line)
-  puts line.split(" ").reverse.join(" ")
-end
-
 filename = ARGV[0]
 file = File.open(filename).each do |line|
-  reverse_words line
+  puts line.split(" ").reverse.join(" ")
 end
 file.close
