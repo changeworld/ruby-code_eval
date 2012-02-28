a45c3b888044ec79c10cf178452dbfe64645a52b
changeworld <changesworlds@gmail.com>
2012-02-29 01:59:02 +0900

    高速処理化

--- lib/22_fibonacci_series/fibonacci_series.rb
+++ lib/22_fibonacci_series/fibonacci_series.rb
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 def fibonacci i
-  i == 0 ? 0 : i <= 2 ? 1 : fibonacci(i - 1) + fibonacci(i - 2)
+  @cache ||= []
+  @cache[i] ||= (i < 2)? i : fibonacci(i - 2) + fibonacci(i - 1)
 end
 
 filename = ARGV[0]
