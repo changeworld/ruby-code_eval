4157b831ae5c9cfc706e92f7923b2ab4eef9d158
changeworld <changesworlds@gmail.com>
2012-03-01 21:11:43 +0900

    変数の統一(no, number を num へ)

--- lib/39_happy_numbers/happy_numbers.rb
+++ lib/39_happy_numbers/happy_numbers.rb
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-def happy_number?(number, cache=[])
-  return 1 if 1 == number
-  cache << number
-  res = sum_of_sq_of_digits(number)
+def happy_number?(num, cache=[])
+  return 1 if 1 == num
+  cache << num
+  res = sum_of_sq_of_digits(num)
   return 0 if cache.include? res
   happy_number?(res, cache)
 end
 
-def sum_of_sq_of_digits(number)
-  number.to_s.each_char.map{|ch| ch.to_i**2}.inject(:+)
+def sum_of_sq_of_digits(num)
+  num.to_s.each_char.map{|ch| ch.to_i**2}.inject(:+)
 end
 
 filename = ARGV[0]
