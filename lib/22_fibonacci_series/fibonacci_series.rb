e802acece59d7896aaddcb2ae27512126ac32139
changeworld <changesworlds@gmail.com>
2013-12-03 00:44:12 +0900

    シンプルに変更(速度は変わらない)

--- lib/22_fibonacci_series/fibonacci_series.rb
+++ lib/22_fibonacci_series/fibonacci_series.rb
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 def fibonacci i
-  @cache ||= []
-  @cache[i] ||= (i < 2)? i : fibonacci(i - 2) + fibonacci(i - 1)
+#  @cache ||= []
+#  @cache[i] ||= (i < 2)? i : fibonacci(i - 2) + fibonacci(i - 1)
+  f0, f1 = 0, 1
+  i.times do
+    f0, f1 = f1, f0+f1
+  end
+  return f0
 end
 
 filename = ARGV[0]
