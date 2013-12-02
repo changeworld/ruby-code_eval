4411684b4c952c0648208e897b3f12827e89593c
changeworld <changesworlds@gmail.com>
2013-12-03 00:51:56 +0900

    高速化

--- lib/22_fibonacci_series/fibonacci_series.rb
+++ lib/22_fibonacci_series/fibonacci_series.rb
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+require 'matrix'
 
 def fibonacci i
 #  @cache ||= []
 #  @cache[i] ||= (i < 2)? i : fibonacci(i - 2) + fibonacci(i - 1)
-  f0, f1 = 0, 1
-  i.times do
-    f0, f1 = f1, f0+f1
-  end
-  return f0
+
+#  f0, f1 = 0, 1
+#  i.times do
+#    f0, f1 = f1, f0+f1
+#  end
+#  return f0
+  (Matrix[[1,1],[1,0]]**i)[0,1]
 end
 
 filename = ARGV[0]
